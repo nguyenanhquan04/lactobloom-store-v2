a210898278016f5fc675461b03edbea312d1164e
nguyenanhquan04 <fpskirito2004@gmail.com>
2024-05-23 21:08:41 +0700

    v2

--- src/components/product/ProductGridListSingle.js
+++ src/components/product/ProductGridListSingle.js
@@ -57,7 +57,7 @@ const ProductGridListSingle = ({
             {product.discount || product.new ? (
               <div className="product-img-badges">
                 {product.discount ? (
-                  <span className="pink">-{product.discount}%</span>
+                  <span className="red">-{product.discount}%</span>
                 ) : (
                   ""
                 )}
